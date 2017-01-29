1bb03cf9c4ed262323ff286b3ad33e46a2b4f622
Jimka Bâtard <ghislain.dugat@free.fr>
2017-01-29 21:08:57 +0100

    commit initial

---
+++ Assets/TurfuRobot/Deplacement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKey (KeyCode.D)) {
+			transform.Translate (Vector2.right * 4f * Time.deltaTime);
+			transform.eulerAngles = new Vector3 (0, 0, 0);
+		}
+
+		if (Input.GetKey (KeyCode.Q)) {
+			transform.Translate (Vector2.right * 4f * Time.deltaTime);
+			transform.eulerAngles = new Vector3 (0, 0, 180);
+		}
+
+		if (Input.GetKey (KeyCode.Z)) {
+			transform.Translate (Vector2.right * 4f * Time.deltaTime);
+			transform.eulerAngles = new Vector3 (0, 0, 90);
+		}
+
+		if (Input.GetKey (KeyCode.S)) {
+			transform.Translate (Vector2.right * 4f * Time.deltaTime);
+			transform.eulerAngles = new Vector3 (0, 0, -90);
+		}
+	}
+}
